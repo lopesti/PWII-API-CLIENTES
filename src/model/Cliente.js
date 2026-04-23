7d520c2907340d0c8ed282a56bb2802a68b45a22
wagnermarques <wagnerdocri@gmail.com>
2026-04-22 22:35:37 -0300

    a melhor aula

---
+++ src/model/Cliente.js
@@ -0,0 +1,14 @@
+class Cliente{
+constructor(id, nome, telefone, endereço){
+    this.id=id;
+    this.nome=nome;
+    this.telefone=telefone;
+    this.endereço=endereço;
+
+
+}
+
+
+}
+
+module.exports=Clientes

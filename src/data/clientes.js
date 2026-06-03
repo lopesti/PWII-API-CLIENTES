dcc12705df75a70656ed5d2aa3e64c578273019c
lopesti <renan.lopes767@gmail.com>
2026-06-02 22:32:32 -0300

    FINAL

---
+++ src/data/clientes.js
@@ -0,0 +1,7 @@
+// src/data/clientes.js
+const clientes = [
+    { id: 1, nome: "João Silva", telefone: "11999999999", endereco: "Rua A, 10" },
+    { id: 2, nome: "Maria Souza", telefone: "11888888888", endereco: "Rua B, 20" }
+];
+
+module.exports = clientes;
